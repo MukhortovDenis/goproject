4af2b454a8080b0a81fe0d5a244a006476893660
Denis Muhortov <muhortovdenis48@gmail.com>
2021-10-01 10:28:58 +0300

    конфиг для дефолтного сервера

---
+++ cmd/web/config.go
@@ -0,0 +1,6 @@
+package main
+
+type Server struct {
+	Port string `yaml:"port" env:"PORT"`
+	Host string `yaml:"host" env:"HOST" env-default:"0.0.0.0"`
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 func main() {
+	cfg := Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
 	mainHandle()
 }
