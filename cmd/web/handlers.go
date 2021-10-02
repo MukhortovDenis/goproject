4e891030b650302a1618254c6877fc0baeffd505
Denis Muhortov <muhortovdenis48@gmail.com>
2021-10-02 17:04:15 +0300

    Все сломал))) Проблемы с переходом на postgres

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"goproject/pkg"
 	"html/template"
 	"net/http"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/jackc/pgx/v4"
 )
 
 var user pkg.User
+var connStr string = "postgres://nigger:nigger@localhost:5432"
 
 // Путь до шаблоном, мб быстрее на пару мгновений, если буду указывать не через переменную
 var dirWithHTML string = "./ui/html/"
@@ -22,16 +25,16 @@ func save(w http.ResponseWriter, r *http.Request) {
 	if login == "" || password == "" {
 		fmt.Fprint(w, "Не все данные введены")
 	}
-	db, err := sql.Open("mysql", "mysql:123@tcp(127.0.0.1:3306)/stoneshop")
+	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
-	insert, err := db.Query(fmt.Sprintf("INSERT INTO `users` (`login`, `password`) VALUES('%s', '%s')", login, password))
+	err := conn.QueryRow(context.Background(), "INSERT INTO users (login, password) VALUES ($1, $2)", login, password)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer insert.Close()
-	defer db.Close()
+	defer conn.Close(context.Background())
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
@@ -42,32 +45,44 @@ func check(w http.ResponseWriter, r *http.Request) {
 	if login == "" || password == "" {
 		fmt.Fprint(w, "Не все данные введены")
 	}
-	db, err := sql.Open("mysql", "mysql:123@tcp(127.0.0.1:3306)/stoneshop")
+	db, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
-	search, err := db.Query(fmt.Sprintf("SELECT * FROM `users` WHERE `login`='%s'", login))
-	if err != nil {
-		fmt.Fprint(w, "Неправильный логин")
-	}
-	for search.Next() {
-		err = search.Scan(&user.ID, &user.Login, &user.Password)
-		if err != nil {
-			panic(err)
-		}
-		if password != user.Password {
-			fmt.Fprint(w, "Неправильный пароль")
-		}
-	}
+	// search, err := db.Query(fmt.Sprintf("SELECT * FROM `users` WHERE `login`='%s'", login))
+	// if err != nil {
+	// 	fmt.Fprint(w, "Неправильный логин")
+	// }
+	// for search.Next() {
+	// 	err = search.Scan(&user.ID, &user.Login, &user.Password)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	if password != user.Password {
+	// 		fmt.Fprint(w, "Неправильный пароль")
+	// 	}
+	// }
 
-	defer search.Close()
-	defer db.Close()
+	// defer search.Close()
+	defer db.Close(context.Background())
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // Страницы, которые отображаются у пользователей
 // Пока нет готового дизайна, новые делать не буду((
 func mainHandle() {
+	cfg := Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Отслеживание сервером статических файлов
+	fsForCss := http.FileServer(http.Dir("./ui/static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fsForCss))
+	//
+	fsForImg := http.FileServer(http.Dir("./ui/img/"))
+	http.Handle("/img/", http.StripPrefix("/img", fsForImg))
 	// Регистрация
 	http.HandleFunc("/signin",
 		func(w http.ResponseWriter, r *http.Request) {
@@ -75,7 +90,10 @@ func mainHandle() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			tmp.Execute(w, user) // нил на энное время
+			err = tmp.Execute(w, user) // нил на энное время
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 
 		})
 	//Вход
@@ -85,7 +103,10 @@ func mainHandle() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			tmp.Execute(w, nil) // нил на энное время
+			err = tmp.Execute(w, nil) // нил на энное время
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 		})
 	// Главная
 	http.HandleFunc("/",
@@ -94,13 +115,18 @@ func mainHandle() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			tmp.Execute(w, nil) // нил на энное время)
+			err = tmp.Execute(w, user) // нил на энное время)
+			if err != nil {
+				fmt.Fprint(w, err)
+			}
 		})
 	// То, что пользователь не увидит, пока только сохранение и проверка записи в бд
 	http.HandleFunc("/save_user", save)
 
 	http.HandleFunc("/check_user", check)
-
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", nil)
+	path := cfg.Host + ":" + cfg.Port
+	err = http.ListenAndServe(path, nil)
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module goproject
 
 go 1.17
 
-
 require github.com/go-sql-driver/mysql v1.6.0
-
