d3d07d3400de1c87331758cfc43ec0d491c5fa58
Denis Muhortov <muhortovdenis48@gmail.com>
2021-10-12 10:00:45 +0300

    Fix

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,27 +6,27 @@ import (
 )
 
 func main() {
-
+	router := mainHandle()
 	// m := &autocert.Manager{
 	// 	Cache:      autocert.DirCache("golang-autocert"),
 	// 	Prompt:     autocert.AcceptTOS,
-	// 	HostPolicy: autocert.HostWhitelist("https://stoneshop.herokuapp.com", "stoneshop.herokuapp.com", "127.0.01.1:443"),
+	// 	HostPolicy: autocert.HostWhitelist("https://stoneshop.herokuapp.com", "stoneshop.herokuapp.com", "127.0.0.1:443"),
 	// }
-	// server := &http.Server{
-	// 	Addr:      ":443",
-	// 	Handler:   handler,
-	// 	TLSConfig: m.TLSConfig(),
-	// }
-	router := mainHandle()
-	// log.Fatal(server.ListenAndServeTLS("", ""))
+	server := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: router,
+		// TLSConfig: m.TLSConfig(),
+	}
+	log.Fatal(server.ListenAndServe())
+
 	// c := make(chan os.Signal, 1)
 	// signal.Notify(c, os.Interrupt)
 	// <-c
 	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	// defer cancel()
 	// server.Shutdown(ctx)
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := http.ListenAndServe(":8080", router)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
