fc2d8c59011faa6072693b4adee325ed09ba74a0
Denis Muhortov <muhortovdenis48@gmail.com>
2021-10-12 10:35:13 +0300

    Добавлени логики завершения сервера

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"goproject/pkg"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
@@ -26,16 +30,13 @@ func main() {
 		Handler: router,
 		// TLSConfig: m.TLSConfig(),
 	}
-	log.Fatal(server.ListenAndServe())
-
-	// c := make(chan os.Signal, 1)
-	// signal.Notify(c, os.Interrupt)
-	// <-c
-	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancel()
-	// server.Shutdown(ctx)
-	// err := http.ListenAndServe(":8080", router)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	go func() {
+		log.Fatal(server.ListenAndServe())
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
 }
