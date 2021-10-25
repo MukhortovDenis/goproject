be09ad3683a78dcbd0a4b370761ba9ecfb155f67
Ilya Buzuev <ilya.buzuev@gmail.com>
2021-10-26 00:53:31 +0300

    некоторые исправления

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#burger').addEventListener('click', function() {
+  document.querySelector('#arrow').addEventListener('click', function() {
     document.querySelector('.user__icon').classList.toggle('rotate')
     document.querySelector('#user__menu').classList.toggle('user__menu_active')
   })
