7c8942131c3150dbb9a65ee74ffa9b5ac239c266
Ilya Buzuev <ilya.buzuev@gmail.com>
2021-11-08 17:29:22 +0300

    request test

---
+++ ui/static/js/theme-toggle.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+  let body = document.querySelector('body');
+  let option1 = document.querySelector('#lightThemeOption');
+  let option2 = document.querySelector('#darkThemeOption');
+
+  if (localStorage.getItem('theme') === 'light') {
+    body.classList.remove('page__body_theme_dark');
+    body.classList.add('page__body_theme_light');
+    option2.removeAttribute('checked');
+    option1.setAttribute('checked', 'checked');
+  } else if (localStorage.getItem('theme') === 'dark') {
+    body.classList.remove('page__body_theme_light');
+    body.classList.add('page__body_theme_dark');
+    option1.removeAttribute('checked');
+    option2.setAttribute('checked', 'checked');
+  }
+
+  option1.addEventListener('click', function() {
+    if (body.classList.contains('page__body_theme_dark')) {
+      body.classList.remove('page__body_theme_dark');
+    }
+    body.classList.add('page__body_theme_light');
+    localStorage.removeItem('theme', 'dark');
+    localStorage.setItem('theme', 'light');
+  });
+
+  option2.addEventListener('click', function() {
+    if (body.classList.contains('page__body_theme_light')) {
+      body.classList.remove('page__body_theme_light');
+    }
+    body.classList.add('page__body_theme_dark');
+    localStorage.removeItem('theme', 'light');
+    localStorage.setItem('theme', 'dark');
+  });
+
+  console.log( 'hi' );
+});
