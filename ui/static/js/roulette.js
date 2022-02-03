6f1dae73b65c5dc33887307b147541a955198621
unknown <ilya.buzuev@mail.ru>
2022-02-04 02:20:21 +0300

    Добавлена рулетка

--- ui/static/js/roulette.js
+++ ui/static/js/roulette.js
@@ -1,60 +1,139 @@
 window.addEventListener('DOMContentLoaded', function() {
+  const stoneList = document.querySelector('.roulette__list');
+  const startButton = document.querySelector('.chest__buy-btn');
+  const prizeBox = document.querySelector('.prize__box');
+  const prizeWindow = document.querySelector('.roulette__prize');
+  const mainWindow = document.querySelector('.main__window');
+  const acceptButton = document.querySelector('.accept__button');
+  const closeButton = document.querySelector('.close-popup__button');
 
   function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  const mainWindow = document.querySelector('.main__window');
-  const rouletteBox = document.querySelector('.roulette__box')
-  const buyButton = document.querySelector('.chest__buy-btn');
-  const rouletteItem = document.querySelector('.roulette__item');
-  const wonText = document.querySelector('.roulette__won-text');
-
-  function anim(num) {
-  
-    if (num < numberList.length) {
-      if (numberList[num] == 0) {
-        rouletteItem.innerHTML = 'Рубин';
-      } else if (numberList[num] == 1) {
-        rouletteItem.innerHTML = 'Изумруд';
-      } else if (numberList[num] == 2) {
-        rouletteItem.innerHTML = 'Алмаз';
-      } else if (numberList[num] == 3) {
-        rouletteItem.innerHTML = '<img class="avatar__img" src="/static/images/common/avatar.jpg" alt="">';
-      } else if (numberList[num] == 4) {
-        rouletteItem.innerHTML = 'Лазурит';
-      } else if (numberList[num] == 95) {
-        rouletteItem.innerHTML = 'Говно';
-        wonText.classList.remove('display-none');
-      }
-      // rouletteItem.innerHTML = numberList[num];
-      setTimeout(anim, 65, ++num);
-    }
-  }
- 
-  let numberList = [];
-
-  buyButton.addEventListener('click', () => {
-    mainWindow.classList.remove('display-none');
-    rouletteBox.classList.remove('display-none');
+  function createRandomList() {
+    let chances = [70, 24, 5, 1];
+    let numberList = [];
+    let randomNumber;
 
     for (let i = 0; i < 100; i++) {
+      randomNumber = getRandom(0, 100);
 
-      numberList.push( getRandom(0, 5) );
-  
+      if (randomNumber >= 30 && randomNumber < 100) {
+        numberList.push(chances[0]);
+      } else if (randomNumber >= 6 && randomNumber < 30) {
+          numberList.push(chances[1]);
+      }  else if (randomNumber >= 1 && randomNumber < 6) {
+          numberList.push(chances[2]);
+      } else if (randomNumber < 1) {
+          numberList.push(chances[3]);
+      }
     }
+
+    numberList[50] = 'Жопа муравья';
+
+    return numberList;
+  }
+
+  function pasteElements(randomList) {
+    for (let i = 0; i < randomList.length; i++) {
+
+      switch(randomList[i]) {
+        case 70:
+          stoneList.innerHTML += `<li class="roulette__item uncommon"><img class="stone__img" src="static/images/common/hatat.png" alt=""></li>`;
+          break;
+        case 24:
+          stoneList.innerHTML += `<li class="roulette__item rare"><img class="stone__img" src="static/images/common/amber.png" alt=""></li>`;
+          break;
+        case 5:
+          stoneList.innerHTML += `<li class="roulette__item legendary"><img class="stone__img" src="static/images/common/ruby.png" alt=""></li>`;
+          break;
+        case 1:
+          stoneList.innerHTML += `<li class="roulette__item arcana"><img class="stone__img" src="static/images/common/avatar.jpg" alt=""></li>`;
+          break;
+        case 'Жопа муравья':
+          stoneList.innerHTML += `<li class="roulette__item arcana"><img class="stone__img" src="static/images/common/avatar.jpg" alt=""></li>`;
+          break;
+      }
+    }
+  }
+
+  function getPx(width, margin, id) {
+    return width * id + margin * id + (getRandom(21, 189));
+  }
+
+  function animate({timing, draw, duration}) {
+    let start = performance.now();
   
-    console.log( numberList );
+    requestAnimationFrame(function animate(time) {
+      let timeFraction = (time - start) / duration;
+
+      if (timeFraction > 1) timeFraction = 1;
   
-    numberList.pop();
-    numberList.push(95);
+      let progress = timing(timeFraction);
   
-    console.log( numberList );
+      draw(progress);
   
-    setTimeout(anim, 65, 0);
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
+  function clearStoneList() {
+    stoneList.style.right = 0;
+    stoneList.innerHTML = '';
+  }
+
+  function showPrize(randomList) {
+    prizeWindow.innerHTML = `
+      <img class="prize__img" src="static/images/common/avatar.jpg" alt="">
+      <div class="prize__name arcana-text">${randomList[50]}</div>
+    `;
+
+    prizeBox.classList.remove('display-none');
+    mainWindow.classList.remove('display-none');
+  }
+
+  startButton.addEventListener('click', function() {
+    let px = getPx(150, 8, 50);
     
+    pasteElements( createRandomList() );
+
+    animate({
+      duration: 8000,
+      timing: function easeOut(timeFraction) {
+        return 1 - Math.pow(1 - timeFraction, 3)
+      },
+      draw: function(progress) {
+        stoneList.style.right = progress * px + 'px';
+      }
+    });
+
+    setTimeout(function() {
+      showPrize( createRandomList() )
+
+      animate({
+        duration: 300,
+        timing: function easyOut(timeFraction) {
+          return 1 - Math.pow(1 - timeFraction, 3)
+        },
+        draw: function(progress) {
+          prizeWindow.style.boxShadow = `0px 0px ${progress * 90}px 10px #ade55c`;
+        }
+      });
+    }, 8200)
+
+    startButton.setAttribute('disabled', 'disabled');
+    closeButton.classList.add('display-none');
   })
 
+  acceptButton.addEventListener('click', function() {
+    prizeBox.classList.add('display-none');
+    
+    clearStoneList();
+
+    startButton.removeAttribute('disabled', 'disabled');
+    closeButton.classList.remove('display-none');
+  })
 });
