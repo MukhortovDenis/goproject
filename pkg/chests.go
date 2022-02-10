ba69d412c4c834dac5daac5fbf0bfee08be33d63
Denis Muhortov <muhortovdenis48@gmail.com>
2022-02-10 19:01:23 +0300

    add prize

--- pkg/chests.go
+++ pkg/chests.go
@@ -6,12 +6,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
 	"text/template"
+	"time"
 )
 
 type ChestList struct {
@@ -31,7 +34,7 @@ type chestInfo struct {
 
 type StoneFromChest struct {
 	Name        string  `json:"stoneName"`
-	StoneChance float32 `json:"stoneChance"`
+	StoneChance float32 `json:"stoneChance,omitempty"`
 	URL         string  `json:"stoneURL"`
 	Rare        string  `json:"stoneRare"`
 }
@@ -100,66 +103,87 @@ func chest() *[]struct {
 	return &chestShop
 }
 
-// func (h *Handler) openChest(w http.ResponseWriter, r *http.Request) {
-// 	session, err := store.Get(r, "session")
-// 	if err != nil {
-// 		log.Print(err)
-// 	}
-// 	if session.Values["userID"] == 0{
-// 		Error := new(Error)
-// 		Error.NewErrorMessage("the unauthorized user")
-// 		body := new(bytes.Buffer)
-// 		err = json.NewEncoder(body).Encode(Error)
-// 		if err != nil {
-// 			log.Print(err)
-// 		}
-// 		fmt.Fprint(w, body)
-// 		return
-// 	}
-// 	if r.URL.Query().Get("id") != "" {
-// 		db, err := sql.Open("postgres", dbConn)
-// 		if err != nil {
-// 			fmt.Fprint(w, err)
-// 			return
-// 		}
-// 		defer db.Close()
-// 		var chestInfo chestInfo
-// 		var chestList ChestList
-// 		err = db.QueryRow("SELECT name, url, price, content, chance FROM chests WHERE id=($1)", r.URL.Query().Get("id")).
-// 			Scan(&chestInfo.chestName, &chestInfo.chestURL, &chestInfo.chestPrice, &chestInfo.chestContent, &chestInfo.chestChance)
-// 		if err != nil {
-// 			fmt.Fprint(w, err)
-// 			return
-// 		}
-// 		chestList.ChestName = chestInfo.chestName
-// 		chestList.ChestURL = chestInfo.chestURL
-// 		chestList.ChestPrice = chestInfo.chestPrice
-// 		sliceChance := strings.Split(chestInfo.chestChance, ",")
-// 		sliceContent := strings.Split(chestInfo.chestContent, ",")
-// 		var wg sync.WaitGroup
-// 		for i := range sliceContent {
-// 			var stone StoneFromChest
-// 			wg.Add(2)
-// 			go func(i int) {
-// 				b, _ := strconv.Atoi(sliceContent[i])
-// 				err = db.QueryRow("SELECT name, url, rare_css FROM stones WHERE id=($1)", b).Scan(&stone.Name, &stone.URL, &stone.Rare)
-// 				if err != nil {
-// 					fmt.Fprint(w, err)
-// 				}
-// 				wg.Done()
-// 			}(i)
-// 			go func(i int) {
-// 				b, _ := strconv.ParseFloat(sliceChance[i], 32)
-// 				c := float32(b)
-// 				stone.StoneChance = c
-// 				wg.Done()
-// 			}(i)
-// 			wg.Wait()
-// 			chestList.ChestContent = append(chestList.ChestContent, stone)
-// 		}
-// 	}
+func (h *Handler) openChest(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "session")
+	if err != nil {
+		log.Print(err)
+	}
+	if session.Values["userID"] == nil {
+		Error := new(Error)
+		Error.NewErrorMessage("Не авторизированный пользователь")
+		body := new(bytes.Buffer)
+		err = json.NewEncoder(body).Encode(Error)
+		if err != nil {
+			log.Print(err)
+		}
+		fmt.Fprint(w, body)
+		return
+	}
+	if r.URL.Query().Get("id") != "" {
+		db, err := sql.Open("postgres", dbConn)
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+		defer db.Close()
+		var content string
+		err = db.QueryRow("SELECT chance FROM chests WHERE id=($1)", r.URL.Query().Get("id")).
+			Scan(&content)
+		if err != nil {
+			fmt.Fprint(w, err)
+			return
+		}
+		sliceChance := strings.Split(content, ",")
+		sliceFloatsLegacy := make([]float64, 0, 8)
+		sliceFloatsModified := make([]float64, 0, 8)
+		for _, i := range sliceChance {
+			num, err := strconv.ParseFloat(i, 32)
+			if err != nil {
+				fmt.Fprint(w, err)
+				return
+			}
+			num = num * 0.01
+			sliceFloatsLegacy = append(sliceFloatsLegacy, num)
+		}
+		sliceFloatsModified = append(sliceFloatsModified, sliceFloatsLegacy...)
+		sort.Float64s(sliceFloatsModified)
+		var winner *int
+		rand.Seed(time.Now().UnixNano())
+		rnd := rand.Float32()
+		for i, j := range sliceFloatsModified {
+			if i == len(sliceFloatsModified)-1 && float32(j) <= rnd && rnd <= 1 {
+				*winner = i
+				break
+			}
+			if rnd <= float32(sliceFloatsModified[0]) {
+				*winner = 0
+				break
+			}
+			if rnd >= float32(j) && rnd <= float32(sliceFloatsModified[i+1]) {
+				*winner = i
+				break
+			}
+		}
+		chance := sliceFloatsModified[*winner]
+		var stoneWinner StoneFromChest
+		for i, j := range sliceFloatsLegacy {
+			if chance == j {
+				if err = db.QueryRow("SELECT name, url, rare_css FROM stones WHERE id=($1)", i).Scan(&stoneWinner.Name, &stoneWinner.URL, &stoneWinner.Rare); err != nil {
+					fmt.Fprint(w, err)
+				}
+				buf := new(bytes.Buffer)
+				if err := json.NewEncoder(buf).Encode(stoneWinner); err != nil {
+					fmt.Fprint(w, err)
+				}
+				w.Write(buf.Bytes())
+				return
+			}
+		}
+		fmt.Fprint(w, "Хуйня какая то")
 
-// }
+	}
+
+}
 
 func (h *Handler) giveChests(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Query().Get("id") != "" {
